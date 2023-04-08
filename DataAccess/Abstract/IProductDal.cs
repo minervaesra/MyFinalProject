3a536e94bcea506e89bcc302e361b912ef0678b6
ASUS <ASUS@DESKTOP-R0T2THI>
2023-04-08 17:27:22 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //ınterface ın kendisi default publicdeğildir ama metodları default public  dir
+    public interface IProductDal:IEntityRepository<Product>
+    {
+       
+    }
+}
